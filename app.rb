4a49d172c3f5c00673defe10d17cb1652f293023
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-02-24 16:55:48 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,9 @@ class App < Sinatra::Base
   end
   
   post '/food' do 
-    params.to_s 
+    post '/food' do
+    "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
+    end
   end 
 
   # Add your post route and action below
